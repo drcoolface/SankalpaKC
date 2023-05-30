7db88609f95e7eaf7edf350d507f778700b7638f
drcoolface <kcsankalpa@gmail.com>
2023-05-30 07:09:29 +0545

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 import { React, useState } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
-import About from './components/About';
+
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Research from './components/About';
